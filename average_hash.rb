df90699da3defa844f723d7f93f1df217081cf32
Hideki Okamoto <hdk531@me.com>
2014-02-17 14:12:14 +0900

    Change "require" from Bundler to RMagic

--- average_hash.rb
+++ average_hash.rb
@@ -21,8 +21,7 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 =end
 
-require 'bundler'
-Bundler.require
+require 'RMagick'
 
 class AverageHash
   def self.calc(filename, size = 16)
